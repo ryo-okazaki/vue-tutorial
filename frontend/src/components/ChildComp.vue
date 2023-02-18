4862a132c0244bd57117ed55adfc5727468650e0
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-02-18 18:28:23 +0900

    公式チュートリアル12/15 Props 子コンポーネント定義まで

--- frontend/src/components/ChildComp.vue
+++ frontend/src/components/ChildComp.vue
@@ -1,3 +1,17 @@
+<script>
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
+<!--
+子コンポーネントは、親コンポーネントからpropsを開始て入力を受け取ることができる
+まず、受け入れるpropsを宣言する必要がある
+
+一旦、宣言されると、msgプロパティはthisに出力され、子コンポーネントのテンプレートで使用することができる
+-->
+
 <template>
-  <h2>A Child Component</h2>
+  <h2>{{ msg || 'No props passed yet' }}</h2>
 </template>
